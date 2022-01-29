0fc9ea141ba40073613178131263a71c0e405189
veeresh <jveereshnaik@gmail.com>
2022-01-29 19:07:49 +0530

    profile-settings activity template implementation

--- app/src/main/java/com/example/project/Activity/MainActivity.java
+++ app/src/main/java/com/example/project/Activity/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         sosBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
